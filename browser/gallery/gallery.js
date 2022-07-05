d43c802c0a78f62f1f1b256904ccf73182c3a533
comradexlight <comradexlight@gmail.com>
2022-07-05 11:48:09 +0300

    hw1

--- browser/gallery/gallery.js
+++ browser/gallery/gallery.js
@@ -8,3 +8,32 @@ const allImg = [
 	"i/new-museum.jpg",
 ];
 
+let counter = 0;
+let element = document.getElementById("currentPhoto");
+const prevPhotoButton = document.getElementById("prevPhoto");
+const nextnextPhoto = document.getElementById("nextPhoto");
+
+
+function nextPhoto () {
+	counter++;
+	if (counter === allImg.length) {
+		counter = 0;
+	};	
+	
+	element.outerHTML = '<img id="currentPhoto" src="'+allImg[counter]+'" alt="">';
+	element = document.getElementById("currentPhoto");
+};
+
+function prevPhoto () {
+	if (counter === 0) {
+		counter = allImg.length;
+	};
+	
+	counter--;
+	element.outerHTML =  '<img id="currentPhoto" src="'+allImg[counter]+'" alt="">';
+	element = document.getElementById("currentPhoto");	
+	
+};
+
+nextnextPhoto.onclick = nextPhoto;
+prevPhotoButton.onclick = prevPhoto;
--- browser/slider/slider.js
+++ browser/slider/slider.js
@@ -16,7 +16,6 @@ let element = document.getElementById("slider");
 setInterval(function() {
 	element.outerHTML = '<img src="'+allImg[counter]+'" alt="" id="slider">';
 	element = document.getElementById("slider");
-	console.log(allImg[counter]);
 	counter++;
 
 
